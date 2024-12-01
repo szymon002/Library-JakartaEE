939be8b4b8e2b6c97b39d3d5af80dc8462f8ac7e
szymon002 <103931397+szymon002@users.noreply.github.com>
2024-12-01 23:32:28 +0100

    Update LoggerInterceptor.java

--- src/main/java/lab/library/configuration/singleton/logger/LoggerInterceptor.java
+++ src/main/java/lab/library/configuration/singleton/logger/LoggerInterceptor.java
@@ -5,18 +5,18 @@ import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 import jakarta.inject.Inject;
 import jakarta.security.enterprise.SecurityContext;
-import lombok.extern.java.Log;
 
+import java.util.logging.Logger;
 import java.util.logging.Level;
 
 
 @LogBinding
 @Interceptor
 @Priority(10)
-@Log
 public class LoggerInterceptor {
 
     private SecurityContext securityContext;
+    static Logger logger = Logger.getLogger(LoggerInterceptor.class.getName());
 
     @Inject
     public LoggerInterceptor(SecurityContext securityContext) {
@@ -25,13 +25,11 @@ public class LoggerInterceptor {
 
     @AroundInvoke
     public Object invoke(InvocationContext context) throws Exception {
-        System.out.println("siema");
         String user = securityContext.getCallerPrincipal().getName();
         String operation = context.getMethod().getName();
         String resource = context.getParameters().length > 0 ? context.getParameters()[0].toString() : "Unknown";
-        log.log(Level.INFO, "User '" + user + "' performed '" + operation + "' on resource with ID '" + resource + "'");
         System.out.println("User '" + user + "' performed '" + operation + "' on resource with ID '" + resource + "'");
-
+        logger.log(Level.INFO, "User '" + user + "' performed '" + operation + "' on resource with ID '" + resource + "'");
         return context.proceed();
     }
 }
